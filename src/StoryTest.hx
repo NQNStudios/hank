850ba1a3e3562b4a0ebff4e5ecfd6d3ac3f3893d
Nat Quayle Nelson <natquaylenelson@gmail.com>
2018-12-23 18:11:03 -0500

    Improving unit tests

--- src/StoryTest.hx
+++ src/StoryTest.hx
@@ -51,27 +51,4 @@ class StoryTest extends haxe.unit.TestCase {
         assertEquals("HasText(That's perfectly valid!)", Std.string(story.nextFrame()));
         assertEquals(StoryFrame.Empty, story.nextFrame());
     }
-
-    public function interactiveTest() { 
-        var story: Story = new Story("examples/main.hank");
-        var frame = StoryFrame.Empty;
-        do {
-            frame = story.nextFrame();
-            switch (frame) {
-                case HasText(text):
-                    trace(text);
-                case HasChoices(choices): 
-                    for (index in 1... choices.length) {
-                        var choice = choices[index-1];
-                        trace("${index}. ${choice}");
-                    }
-
-                    // TODO accept choice and follow it
-                default:
-            }
-
-        } while (frame != Empty);
-
-        trace("Story is finished.");
-    }
 }
