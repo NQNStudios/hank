bd61915e5f138ab4a7f1a42a3045300b03ed22e3
Nat Quayle Nelson <natquaylenelson@gmail.com>
2018-12-23 21:58:01 -0500

    Test viewcounts (close #3)

--- src/StoryTest.hx
+++ src/StoryTest.hx
@@ -1,5 +1,7 @@
 package src;
 
+import src.Story.HankLine;
+
 class StoryTest extends haxe.unit.TestCase {
     public static function main() {
         var r = new haxe.unit.TestRunner();
@@ -13,7 +15,7 @@ class StoryTest extends haxe.unit.TestCase {
     public function testParseHelloWorld() {
         var story: Story = new Story();
         story.loadScript("examples/hello.hank");
-        assertTrue(Std.string(story.scriptLines[0]).indexOf('type: OutputText(Hello, world!)') != -1);
+        assertLineType('OutputText(Hello, world!)', story.scriptLines[0]);
     }
 
     public function testHelloWorld() {
@@ -123,7 +125,7 @@ class StoryTest extends haxe.unit.TestCase {
     }
 
     public function testViewCounts() {
-        var story = new Story();
+        var story = new Story(true);
         story.loadScript("examples/main.hank");
 
         assertEquals(0, story.interp.variables['start']);
@@ -135,10 +137,14 @@ class StoryTest extends haxe.unit.TestCase {
 
     public function testParseLine() {
         var story = new Story();
-        assertEquals("IncludeFile(extra.hank)", Std.string(story.parseLine("INCLUDE extra.hank", [])));
-        assertEquals("IncludeFile(extra.hank)", Std.string(story.parseLine("INCLUDE    extra.hank", [])));
+        assertEquals("IncludeFile(examples/extra.hank)",Std.string(story.parseLine("INCLUDE examples/extra.hank", [])));
 
         // TODO test edge cases of all line types (maybe with more separate functions too)
+    }
+
+    private function assertLineType(type: String, line: HankLine) {
+        var actual = Std.string(line.type);
+        assertEquals(type, actual);
     }
 
     public function testParseFullSpec() {
@@ -149,40 +155,55 @@ class StoryTest extends haxe.unit.TestCase {
 
         // TODO test a few line numbers from the script to make sure the parsed versions match. Especially block line numbers
 
-        var idx = 0;
-        trace(Std.string(story.scriptLines[idx]));
-        assertTrue(Std.string(story.scriptLines[idx++]).indexOf('type: IncludeFile(examples/extra.hank)') != -1);
-        assertTrue(Std.string(story.scriptLines[idx++]).indexOf('type: Divert(start)') != -1);
-        assertTrue(Std.string(story.scriptLines[idx++]).indexOf('type: DeclareSection(start)') != -1);
-        assertTrue(Std.string(story.scriptLines[idx++]).indexOf('type: OutputText(This is a section of a Hank story. It\'s pretty much like a Knot in Ink.)') != -1);
-        assertTrue(Std.string(story.scriptLines[idx++]).indexOf('type: OutputText(Line breaks define the chunks of this section that will eventually get sent to your game to process!)') != -1);
-        assertTrue(Std.string(story.scriptLines[idx++]).indexOf('type: OutputText(Your Hank scripts will contain the static content of your game, but they can also insert {demo_var}, even the result of complex {part1 + " " + part2}!)') != -1);
-        assertTrue(Std.string(story.scriptLines[idx++]).indexOf('type: HaxeLine(var multiline_logic = "Logic can happen on any line before a multiline comment.";)') != -1);
-        assertTrue(Std.string(story.scriptLines[idx++]).indexOf('type: BlockComment(3)') != -1 );
-        // trace(Std.string(story.scriptLines[idx]));
-        assertTrue(Std.string(story.scriptLines[idx++]).indexOf('type: HaxeLine(multiline_logic_example = "Logic can happen on any line after a multiline comment.";)') != -1);
-        assertTrue(Std.string(story.scriptLines[idx++]).indexOf('type: Divert(choice_example)') != -1);
-        assertTrue(Std.string(story.scriptLines[idx++]).indexOf('type: DeclareSection(final_choice)') != -1);
-        //trace(Std.string(story.scriptLines[idx]));
-        assertTrue(Std.string(story.scriptLines[idx++]).indexOf('type: HaxeBlock(5,var unused_variable="";\n// This is a comment INSIDE a haxe block\n/*The whole block will be parsed and executed at the same time*/\n)') != -1);
-        //trace(Std.string(story.scriptLines[idx]));
-        assertTrue(Std.string(story.scriptLines[idx++]).indexOf('type: DeclareChoice({text: I don\'t think I\'ll use Hank for my games., id: 0, depth: 1, expires: true})') != -1);
-        assertTrue(Std.string(story.scriptLines[idx++]).indexOf('type: OutputText(Are you sure?)') != -1);
-        // trace(Std.string(story.scriptLines[idx]));
-        assertTrue(Std.string(story.scriptLines[idx++]).indexOf("type: DeclareChoice({text: Yes I'm sure., id: 1, depth: 2, expires: true})") != -1);
-        assertTrue(Std.string(story.scriptLines[idx++]).indexOf('type: OutputText(That\'s perfectly valid!)') != -1);
-        // trace(Std.string(story.scriptLines[idx]));
-        assertTrue(Std.string(story.scriptLines[idx++]).indexOf("type: Divert(the_end)") != -1);
-        assertTrue(Std.string(story.scriptLines[idx++]).indexOf("type: DeclareChoice({text: I've changed my mind., id: 2, depth: 2, expires: true})") != -1);
-        assertTrue(Std.string(story.scriptLines[idx++]).indexOf('type: Divert(final_choice)') != -1);
-        assertTrue(Std.string(story.scriptLines[idx++]).indexOf('type: DeclareChoice({text: Hank sounds awesome, thanks!, id: 3, depth: 1, expires: true})') != -1);
-        assertTrue(Std.string(story.scriptLines[idx++]).indexOf('type: Divert(the_end)') != -1);
-        assertTrue(Std.string(story.scriptLines[idx++]).indexOf('type: DeclareSection(the_end)') != -1);
-        assertTrue(Std.string(story.scriptLines[idx++]).indexOf('type: OutputText(That\'s the end of this example!)') != -1);
-        // trace(Std.string(story.scriptLines[idx]));
-        assertTrue(Std.string(story.scriptLines[idx++]).indexOf('type: OutputText(This should say \'mouse\': {what_happened}') != -1);
+        // TODO test the extra.hank lines
 
 
-        // Parse the extra.hank script and also test its parsing
+        var lineTypes = [
+            // TODO the 22 lines of the extra.hank file
+            'IncludeFile(examples/extra.hank)',
+            'Empty',
+            'Divert(start)',
+            'Empty',
+            'DeclareSection(start)',
+            'OutputText(This is a section of a Hank story. It\'s pretty much like a Knot in Ink.)',
+            'OutputText(Line breaks define the chunks of this section that will eventually get sent to your game to process!)',
+            'OutputText(Your Hank scripts will contain the static content of your game, but they can also insert {demo_var}, even the result of complex {part1 + " " + part2}!)',
+            'Empty',
+            'HaxeLine(var multiline_logic = "Logic can happen on any line before a multiline comment.";)',
+            'BlockComment(3)',
+            'Empty',
+            'Empty',
+            'HaxeLine(multiline_logic_example = "Logic can happen on any line after a multiline comment.";)',
+            'Empty',
+            'Divert(choice_example)',
+            'Empty',
+            'DeclareSection(final_choice)',
+            'Empty',
+            'HaxeBlock(5,var unused_variable="";\n// This is a comment INSIDE a haxe block\n/*The whole block will be parsed and executed at the same time*/\n)',
+            'Empty',
+            'Empty',
+            'Empty',
+            'Empty',
+            'DeclareChoice({text: I don\'t think I\'ll use Hank for my games., id: 3, depth: 1, expires: true})',
+            'OutputText(Are you sure?)',
+            'DeclareChoice({text: Yes I\'m sure., id: 4, depth: 2, expires: true})',
+            'OutputText(That\'s perfectly valid!)',
+            'Divert(the_end)',
+            'DeclareChoice({text: I\'ve changed my mind., id: 5, depth: 2, expires: true})',
+            'Divert(final_choice)',
+            'DeclareChoice({text: Hank sounds awesome, thanks!, id: 6, depth: 1, expires: true})',
+            'Divert(the_end)',
+            'Empty',
+            'DeclareSection(the_end)',
+            'Empty',
+            'OutputText(That\'s the end of this example!)',
+            'OutputText(This should say \'mouse\': {what_happened})'
+        ];
+
+        var idx = 22;
+        var i = 0;
+        while (idx < story.scriptLines.length) {
+            assertLineType(lineTypes[i++], story.scriptLines[idx++]);
+        }
     }
 }
