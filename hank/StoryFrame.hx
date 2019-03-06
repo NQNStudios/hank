e3049039c4a5104363aab161be89911cf360f287
Nat Quayle Nelson <natquaylenelson@gmail.com>
2019-03-06 16:24:17 -0500

    Throwing out the spaghetti code

--- hank/StoryFrame.hx
+++
@@ -1,9 +0,0 @@
-package hank;
-
-enum StoryFrame {
-    HasText(text: String);
-    HasChoices(choices: Array<String>);
-    Error(message: String);
-    Empty; // Returned by Hank statements that have no output
-    Finished; // Returned when the Hank story is finished or has crashed
-}
