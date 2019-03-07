3410ce7baaac903ff96631b22107fa9b8617bdb9
Nat Quayle Nelson <natquaylenelson@gmail.com>
2019-03-06 22:00:01 -0500

    WIP confusing recursive data structure for ViewCounts

---
+++ hank/ViewCounts.hx
@@ -0,0 +1,17 @@
+/*
+package hank;
+
+import haxe.ds.Option;
+
+/**
+ Represents a scope of ViewCounts.
+**/
+class ViewCounts {
+
+    var counts: Map<String, Int> = new Map();
+    var scopes: Map<String, ViewCounts> = new Map();
+
+    // TODO need to define expected behavior for view counts. For example, each time a stitch in a section is viewed, does that increment the section's view count? Do view counts increment when a section loops back on itself, or only when the scope changes from a distinct one to this one? 
+
+}
+*/
