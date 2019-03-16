df878b440990e9498dd3d1adca8afcb06efca1cd
Nat Quayle Nelson <natquaylenelson@gmail.com>
2019-03-15 22:15:11 -0600

    FileBuffer handles comments internally

--- hank/FileBuffer.hx
+++ hank/FileBuffer.hx
@@ -11,18 +11,43 @@ typedef Position = {
     var column: Int;
 }
 
+typedef BufferOutput = {
+    output: String,
+    terminator: String
+};
+
 /**
- Helper class for reading/parsing information from a file.
+ Helper class for reading/parsing information from a file. Completely drops comments
 **/
 class FileBuffer {
     var path: String;
-    var buffer: String;
+    var cleanBuffer: String;
+    var rawBuffer: String;
     var line: Int = 1;
-    var column: Int = 0;
+    var column: Int = 1;
 
     public function new(path: String) {
-        buffer = sys.io.File.getContent(path);
+        // Keep a raw buffer of the file for tracking accurate file positions
+        rawBuffer = sys.io.File.getContent(path);
+        // Keep a clean buffer for returning data without comments getting in the way
+        cleanBuffer = stripComments(rawBuffer, '//', '\n', false);
+        cleanBuffer = stripComments(cleanBuffer, '/*', '*/', true);
+
         this.path = path;
+    }
+
+    function stripComments(s: String, o: String, c: String, dc: Bool): String {
+        while (s.indexOf(o) != -1) {
+            var start = s.indexOf(o);
+            var end = s.indexOf(c, start);
+            if (end == -1) {
+                s = s.substr(0, start);
+            } else {
+                if (dc) end += c.length;
+                s = s.substr(0, start) + s.substr(end);
+            }
+        }
+        return s;
     }
 
     public function position(): Position {
@@ -32,66 +57,114 @@ class FileBuffer {
             column: column
         };
     }
-
     /** Peek through the buffer until encountering one of the given terminator sequences
     @param eofTerminates Whether the end of the file is also a valid terminator
     **/
-    public function peekUntil(terminators: Array<String>, eofTerminates: Bool = false): Option<String> {
+    public function peekUntil(terminators: Array<String>, eofTerminates: Bool = false, raw: Bool = false): Option<BufferOutput> {
+        var buffer = raw ? rawBuffer : cleanBuffer;
         if (buffer.length == 0) return None;
 
         var index = buffer.length;
 
+        var whichTerminator = '';
         for (terminator in terminators) {
             var nextIndex = buffer.indexOf(terminator);
-            index = (nextIndex != -1 && nextIndex < index) ? nextIndex : index;
+            if (nextIndex != -1 && nextIndex < index) {
+                index = nextIndex;
+                whichTerminator = terminator;
+            }
         }
 
         return if (index < buffer.length || eofTerminates) {
-            Some(buffer.substr(0, index));
+            Some({
+                output: buffer.substr(0, index),
+                terminator: whichTerminator
+            });
         } else {
             None;
         }
     }
 
     /**
-     Drop the given string from the front of buffer, updating the current position
+     Drop the given string from the front of the raw buffer, updating the current position according to the raw buffer
     **/
-    public function drop(s: String) {
+    function dropRaw(s: String) {
+        var actual = rawBuffer.substr(0, s.length);
+        if (actual != s) {
+            throw 'Expected to drop "${s}" but was "${actual}"';
+        }
+
+
         var lines = s.split('\n');
         if (lines.length > 1) {
             line += lines.length - 1;
-            column = lines[lines.length -1].length;
+            column = lines[lines.length -1].length+1;
         } else {
-            column += lines[lines.length-1].length;
-        }
-        var dropped = buffer.substr(0, s.length);
-        if (dropped != s) {
-            throw 'FileBuffer drop error at ${position()}: Expected to drop ${s} but was ${dropped}';
+            column += lines[0].length;
         }
 
-        buffer = buffer.substr(s.length);
+        rawBuffer = rawBuffer.substr(s.length);
+    }
+
+    /** Drop text directly from the clean buffer **/
+    function dropClean(s: String) {
+        var actual = cleanBuffer.substr(0, s.length);
+
+        if (actual != s) {
+            throw 'Expected to drop "${s}" but was "${actual}"';
+        }
+
+        cleanBuffer = cleanBuffer.substr(s.length);
+    }
+
+    /**
+     Drop the given string from the front of the unified buffer object, keeping the two back-end buffers synchronized
+    **/
+    public function drop(s: String) {
+        var untilNextComment = peekUntil(['//', '/*'], false, true);
+
+        switch (untilNextComment) {
+            case Some({output: rawS, terminator: commentOpener}) if (rawS.length < s.length):
+                var commentTerminator = switch (commentOpener) {
+                    case '//': '\n';
+                    case '/*': '*/';
+                    default: throw 'FUBAR';
+                }
+                dropRaw(rawS + commentOpener);
+                dropClean(rawS);
+                var untilEndOfComment = peekUntil([commentTerminator], true, true);
+                switch (untilEndOfComment) {
+                    case Some({output: comment, terminator: _}):
+                        dropRaw(comment);
+                        if (commentTerminator != '\n') {
+                            dropRaw(commentTerminator);
+                        }
+                        // Drop the rest of the clean sequence
+                        var rest = s.substr(rawS.length);
+
+                        drop(rest);
+                    default: throw 'FUBAR';
+                }
+            default:
+                dropClean(s);
+                dropRaw(s);
+        }
     }
 
     /** Take data from the file until encountering one of the given terminator sequences. **/
-    public function takeUntil(terminators: Array<String>, eofTerminates: Bool = false, dropTerminator = true): Option<String> {
+    public function takeUntil(terminators: Array<String>, eofTerminates: Bool = false, dropTerminator = true): Option<BufferOutput> {
         return switch (peekUntil(terminators, eofTerminates)) {
-            case Some(s):
+            case Some({output: s, terminator: t}):
                 // Remove the desired data from the buffer
                 drop(s);
 
                 // Remove the terminator that followed the data from the buffer
-                var nextTerminator = '';
-                for (terminator in terminators) {
-                    if (buffer.indexOf(terminator) == 0) {
-                        if (dropTerminator) {
-                            drop(terminator);
-                        }
-                        break;
-                    }
+                if (dropTerminator) {
+                    drop(t);
                 }
 
                 // Return the desired data
-                Some(s);
+                Some({output: s, terminator: t});
             case None:
                 None;
         }
@@ -99,28 +172,21 @@ class FileBuffer {
 
     /** Take the specified number of characters from the file **/
     public function take(chars: Int) {
-        var data = buffer.substr(0, chars);
+        if (cleanBuffer.length < chars) {
+            throw 'Not enough characters left in buffer.';
+        }
+        var data = cleanBuffer.substr(0, chars);
         drop(data);
         return data;
     }
 
-    /** Take the next line of data from the file. (Stops for comments)
+    /** Take the next line of data from the file.
     @param trimmed Which sides of the line to trim ('r' 'l', 'lr', or 'rl')
     **/
     public function takeLine(trimmed = ''): Option<String> {
-        var nextLine = if (StringTools.startsWith(buffer, '//') || StringTools.startsWith(buffer, '/*')) {
-            takeUntil(['\n'], true);
-        } else {
-            switch (peekWhichComesNext(['\n', '//', '/*'])) {
-                case Some([0, _]) | None:
-                    takeUntil(['\n'], true);
-                // Preserve // and /* if they terminate a line
-                default:
-                    takeUntil(['//', '/*'], false, false);
-            }
-        }
+        var nextLine = takeUntil(['\n'], true);
         return switch (nextLine) {
-            case Some(nextLine):
+            case Some({output: nextLine, terminator: _}):
                 if (trimmed.indexOf('r') != -1) {
                     nextLine = StringTools.rtrim(nextLine);
                 }
@@ -131,29 +197,5 @@ class FileBuffer {
             case None:
                 None;
         }
-    }
-
-    public function peekWhichComesNext(terminators: Array<String>): Option<Array<Int>> {
-        var peek = peekUntil(terminators);
-        switch (peek) {
-            case Some(peekedPast):
-                var index = peekedPast.length;
-                var which = -1;
-                for (i in 0...terminators.length) {
-                    var terminator = terminators[i];
-                    if (StringTools.startsWith(buffer.substr(index), terminator)) {
-                        which = i;
-                        break;
-                    }
-                }
-                return Some([which, index]);
-            case None:
-                return None;
-        }
-    }
-
-    /** Give text back to the buffer **/
-    public function give(s: String) {
-        buffer = s + buffer;
     }
 }
--- tests/FileBufferTest.hx
+++ tests/FileBufferTest.hx
@@ -6,77 +6,90 @@ import hank.FileBuffer.Position;
 
 class FileBufferTest extends utest.Test {
     var file: FileBuffer;
+    var path: String;
+    var expectedPosition: Position;
 
     function setup() {
-        file = new FileBuffer('examples/main/main.hank');
+        path = 'examples/parsing/file.txt';
+        file = new FileBuffer(path);
+        expectedPosition = {
+            file: path,
+            line: 1,
+            column: 1
+        };
+    }
+
+    function assertPosition() {
+        HankAssert.equals(expectedPosition, file.position());
     }
 
     function testPeek() {
-        var expectedPosition = {
-            file: 'examples/main/main.hank',
-            line: 1,
-            column: 0
-        };
-        HankAssert.equals(expectedPosition, file.position());
-        HankAssert.equals(Some('INCLUDE'), file.peekUntil(" ".split("")));
+        assertPosition();
+        HankAssert.equals(Some({ output: 'Line', terminator: " "}), file.peekUntil(" ".split("")));
         // Repeated calls should return the same result
-        HankAssert.equals(Some('INCLUDE'), file.peekUntil(" ".split("")));
+        HankAssert.equals(Some({ output: 'Line', terminator: " "}), file.peekUntil(" ".split("")));
         // And leave position unmodified
-        HankAssert.equals(expectedPosition, file.position());
+        assertPosition();
 
         // Once more for good measure
-        HankAssert.equals(Some('INCLUDE'), file.peekUntil(" ".split("")));
-        HankAssert.equals(expectedPosition, file.position());
+        HankAssert.equals(Some({ output: 'Line', terminator: " "}), file.peekUntil(" ".split("")));
+        assertPosition();
     }
 
     function testTake() {
-        var expectedPosition1 = {
-            file: 'examples/main/main.hank',
-            line: 1,
-            column: 0
-        };
-        var expectedPosition2 = {
-            file: 'examples/main/main.hank',
-            line: 1,
-            column: 8
-        };
-        var expectedPosition3 = {
-            file: 'examples/main/main.hank',
-            line: 2,
-            column: 0
-        };
+        var twoLines = file.take(27+20);
+        HankAssert.equals("Line of text.\nLine of text  without a comment.\n", twoLines);
+        expectedPosition.line = 3;
+        expectedPosition.column = 1;
+        assertPosition();
+    }
 
-        HankAssert.equals(expectedPosition1, file.position());
-        HankAssert.equals(Some('INCLUDE'), file.takeUntil(" ".split("")));
-        HankAssert.equals(expectedPosition2, file.position());
-        HankAssert.equals(Some('extra.hank'), file.takeUntil("\n".split("")));
-        HankAssert.equals(expectedPosition3, file.position());
+    function testTakeUntil() {
+        assertPosition();
+
+        HankAssert.equals(Some({ output: 'Line', terminator: " "}), file.takeUntil(" ".split("")));
+        expectedPosition.column += 5; // The terminator is dropped by default
+        assertPosition();
+
+        HankAssert.equals(Some({ output: 'of text.', terminator: "\n"}), file.takeUntil("\n".split("")));
+        expectedPosition.line = 2; 
+        expectedPosition.column = 1;
+        assertPosition();
+
+        HankAssert.equals(Some({ output: 'Line', terminator: " "}), file.takeUntil(" ".split(""), false, false));
+        expectedPosition.column += 4; // The terminator doesn't have to be dropped
+        assertPosition();
+
+        HankAssert.equals(Some({ output: ' of text  without a comment.', terminator: '\n'}), file.takeUntil('\n'.split("")));
+        expectedPosition.line = 3;
+        expectedPosition.column = 1;
+        assertPosition();
     }
 
     function testTakeLine() {
-        var expectedPosition1 = {
-            file: 'examples/main/main.hank',
-            line: 1,
-            column: 0
-        };
-        var expectedPosition2 = {
-            file: 'examples/main/main.hank',
-            line: 2,
-            column: 0
-        };
+        assertPosition();
 
-        HankAssert.equals(expectedPosition1, file.position());
-        HankAssert.equals(Some('INCLUDE extra.hank'), file.takeLine());
-        HankAssert.equals(expectedPosition2, file.position());
-    }
+        HankAssert.equals(Some('Line of text.'), file.takeLine());
+        expectedPosition.line += 1;
+        assertPosition();
 
-    function testTakeLineEOF() {
-        var buffer = new FileBuffer('.travis.yml');
-        buffer.takeLine();
-        buffer.takeLine();
-        buffer.takeLine();
-        buffer.takeLine();
-        buffer.takeLine();
-        HankAssert.equals(None, buffer.takeLine());
+        HankAssert.equals(Some('Line of text  without a comment.'), file.takeLine());
+        expectedPosition.line += 1;
+        assertPosition();
+
+        HankAssert.equals(Some('Two lines of text  that will be returned at the same time.'), file.takeLine());
+        expectedPosition.line += 2;
+        assertPosition();
+
+        HankAssert.equals(Some('The fifth line of text. '), file.takeLine());
+        expectedPosition.line += 1;
+        assertPosition();
+
+        HankAssert.equals(Some('Number six.'), file.takeLine());
+        expectedPosition.column += 11;
+        assertPosition();
+
+        // EOF
+        HankAssert.equals(None, file.takeLine());
     }
 }
