ef8c8631e5da168458aa075906ddeb8b4f002aa6
Nat Quayle Nelson <natquaylenelson@gmail.com>
2019-06-06 10:35:04 -0600

    testing bash functions use $VISUAL

--- functions.sh
+++ functions.sh
@@ -1,16 +1,16 @@
 t() {
     haxe -cp hank -lib utest -lib hscript hxml/$1.hxml --interp > test-output.txt
-    vim test-output.txt
+    $VISUAL test-output.txt
 }
 
 ta() {
     haxe -cp hank -lib utest -lib hscript hxml/all-platforms.hxml > test-output.txt
-    vim test-output.txt
+    $VISUAL test-output.txt
 }
 
 tas() {
     haxe -D stop_on_error -cp hank -lib utest -lib hscript hxml/all-platforms.hxml > test-output.txt
-    vim test-output.txt
+    $VISUAL test-output.txt
 }
 
 debug() {
